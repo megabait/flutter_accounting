48eff9f42a304d7b1246ca751f515f22d8ace506
info@megabait.kz <megapass8898>
2022-01-18 22:18:15 +0600

    test_ios_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-//Привет - я работаю на ios)))) dfdfg
+//Привет - я работаю на ios)))) dfdfgsdffs
 void main() {
   runApp(
        MaterialApp(
